272519a46afe5da0004dfebdb0b9b841158d75dc
moises0142 <lopez.moises0142@gmail.com>
2024-04-10 19:36:51 -0400

    made it display wins losses and ties

--- legame.js
+++ legame.js
@@ -5,50 +5,90 @@ function getComputerChoice(){
     return compChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     let result='';
+    let computerSelection=getComputerChoice();
+    let wins=0;
+    let losses=0;
+    let tied=0;
     if( computerSelection === playerSelection){
         result = "It's tie";
+        tied++;
+
     }
     else if(computerSelection === "rock" && playerSelection === "paper"){
         result = "You win";
+        wins++;
     }
     else if(computerSelection === "scissors" && playerSelection === "rock"){
-        result = "You win";    
+        result = "You win";  
+        wins++;
+  
     }
     else if(computerSelection === "paper" && playerSelection === "scissors"){
         result = "You win";
+        wins++;
+
     }
     else{
         result= "You lose"
+        losses++;
+
     }
-    console.log(result);
-    return result;
+    
+    
+    console.log(wins, losses, tied);
+    return [wins, losses, tied];
     
 }
-let playerSelection;
 
 const rockButton = document.querySelector("#Rock");
+const paperButton = document.querySelector("#Paper");
+const scissorsButton = document.querySelector("#Scissors");
+const container = document.querySelector("#div1");
+const container2 = document.querySelector("#div2");
+const container3 = document.querySelector("#div3");
+const winLossContainer = document.querySelector("#winLoss")
+
+let wins = document.createElement("div");
+let loss = document.createElement("div");
+let tie = document.createElement("div");
+let winOrLose = document.createElement("div")
+
 rockButton.addEventListener("click", () => {
     playerSelection="rock";
-    computerSelection=getComputerChoice();
-    playRound(playerSelection,computerSelection);
+    playGame();
+
+
 })
 
-const paperButton = document.querySelector("#Paper");
 paperButton.addEventListener("click", () => {
-    paperSelection="paper";
-    computerSelection=getComputerChoice();
-    playRound(playerSelection,computerSelection);
+    playerSelection="paper";
+    playGame();
 
 })
 
-const scissorsButton = document.querySelector("#Scissors");
 scissorsButton.addEventListener("click", () =>{
     playerSelection="scissors";
-    computerSelection=getComputerChoice();
-    playRound(playerSelection,computerSelection);
+    playGame();
 })
 
+function playGame(){
+
+    let [win,losses,tied]= playRound();
+
+    if(+wins.textContent>=5){
+    winOrLose.textContent="you win";
+    winLossContainer.appendChild(winOrLose);
+    
+    }
+    wins.textContent= +wins.textContent +win;
+    loss.textContent= +loss.textContent + +losses;
+    tie.textContent=  +tie.textContent + +tied;
 
 
+    container.appendChild(wins);
+    container2.appendChild(loss);
+    container3.appendChild(tie);
+
+}
